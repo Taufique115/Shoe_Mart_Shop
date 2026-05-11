c56ee88af0f614498c730cddb834d22341d55d74
Mostafa Jaman Taufique <mostafataufique@gmail.com>
2026-05-11 15:10:18 +0600

    homepage

---
+++ app/src/main/java/com/example/shoemartshop/Activity/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.example.shoemartshop.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.shoemartshop.MainActivity
+import com.example.shoemartshop.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }
+    }
+}
